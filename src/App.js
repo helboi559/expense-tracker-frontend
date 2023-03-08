c21eab39143500531f0d77c00f9bdf5dbe71861e
Juan Ramirez <rami8224.2007@gmail.com>
2023-03-08 16:54:30 -0500

    implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react"
+import React, { Suspense} from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import UserDashboard from "./pages/dashboard/UserDashboard"
+// import Home from "./pages/Home"
+// import UserDashboard from "./pages/dashboard/UserDashboard"
 import CustomTheme from "./CustomTheme"
+const Home = React.lazy(() => import("./pages/Home"))
+const UserDashboard = React.lazy(() => import("./pages/dashboard/UserDashboard"))
 
 const App = () => {
   return (
     <>
-    <CustomTheme>
-      <BrowserRouter>
-        <Routes>
-          <Route path="dashboard/*" element={<UserDashboard/>}/>
-          <Route path="*" element={<Home/>}/>
-        </Routes>
-      
-      </BrowserRouter>
-    </CustomTheme>
+    <Suspense fallback={<div>Loading...</div>}>
+      <CustomTheme>
+        
+        <BrowserRouter>
+          <Routes>
+            <Route path="dashboard/*" element={<UserDashboard/>}/>
+            <Route path="*" element={<Home/>}/>
+          </Routes>
+        
+        </BrowserRouter>
+      </CustomTheme>
+    </Suspense>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import { lazy, Suspense } from "react"
 import {createRoot} from "react-dom/client"
+
 import App from "./App"
-import ContextProvider from "./context/ContextProvider"
+// import ContextProvider from "./context/ContextProvider"
+const ContextProvider = lazy (() => import("./context/ContextProvider"))
 
 createRoot(document.getElementById('root')).render(
-<ContextProvider>
-    <App></App>
-</ContextProvider>)
+<Suspense fallback={<div>Loading...</div>}>
+    <ContextProvider>
+        <App></App>
+    </ContextProvider>
+</Suspense>)
